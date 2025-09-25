565cfbbd22337d2946a80c74d638c425adf2c32e
Maksym Voznyi <mak7kas02@gmail.com>
2025-09-25 17:51:32 +0300

    [+] Move A/D (left and right) [+] Prototype 2

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalInput;
+    public float speed = 15.0f;
+    private float xRange = 14.0f;
+    
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+        
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+    }
+}
