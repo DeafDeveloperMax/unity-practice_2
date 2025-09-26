22b4e10afd5d14a1015466eb29edd51b513759b0
Maksym Voznyi <mak7kas02@gmail.com>
2025-09-26 18:57:55 +0300

    [+] Debug.Log

--- Assets/Scripts/DestroyOutOfBounds.cs
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -15,7 +15,10 @@ public class DestroyOutOfBounds : MonoBehaviour
         if (transform.position.z > topBound)
         {
             Destroy(gameObject);
-        } else if (transform.position.z < lowerBound) {
+        }
+        else if (transform.position.z < lowerBound) 
+        {
+            Debug.Log("GAME OVER! NOOB HA-HA-HA");
             Destroy(gameObject);
         }
     }
